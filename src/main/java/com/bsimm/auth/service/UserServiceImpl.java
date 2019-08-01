82f249883d62726267bd0ef173890bbce3140128
skokil <skokil@cigital.com>
2019-08-01 12:01:38 -0400

    added vuln code samples

--- src/main/java/com/bsimm/auth/service/UserServiceImpl.java
+++ src/main/java/com/bsimm/auth/service/UserServiceImpl.java
@@ -1,16 +1,17 @@
 package com.bsimm.auth.service;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bsimm.auth.model.User;
+import com.bsimm.auth.model.UserRowMapper;
 import com.bsimm.auth.repository.RoleRepository;
 import com.bsimm.auth.repository.UserRepository;
-
-import java.util.HashSet;
-
-import javax.persistence.TypedQuery;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,6 +22,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
@@ -30,8 +34,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByUsername(String username) {
+    	 
+    	String sql = "SELECT username, password FROM user where username='" + username + "'";
+    	RowMapper<User> rowMapper = new UserRowMapper();
     	
-    	//https://github.com/eugenp/tutorials/blob/master/software-security/sql-injection-samples/src/main/java/com/baeldung/examples/security/sql/AccountDAO.java  
-        return userRepository.findByUsername(username);
+    	return jdbcTemplate.query(sql, rowMapper).get(0);
+    	//return userRepository.findByUsername(username);
     }
 }
