1a998c852b4e0fbc9bc95dff946b8b27876e34d4
skokil <skokil@cigital.com>
2020-01-13 14:58:31 -0500

    DEVSECOPS-1234 fix

--- src/main/java/com/bsimm/auth/validator/UserValidator.java
+++ src/main/java/com/bsimm/auth/validator/UserValidator.java
@@ -9,7 +9,7 @@ import org.springframework.validation.Validator;
 import com.bsimm.auth.model.User;
 import com.bsimm.auth.service.UserService;
 
-//this is a pre-push test 3
+//this is a pre-push test 4
 @Component
 public class UserValidator implements Validator {
     @Autowired
