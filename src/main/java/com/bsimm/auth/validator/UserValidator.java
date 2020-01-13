5bfb22d07818a7cb8eabc176512d1dc2575b73d8
skokil <skokil@cigital.com>
2020-01-13 15:21:49 -0500

    DEVSECOPS-3333

--- src/main/java/com/bsimm/auth/validator/UserValidator.java
+++ src/main/java/com/bsimm/auth/validator/UserValidator.java
@@ -36,7 +36,7 @@ public class UserValidator implements Validator {
         if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
             errors.rejectValue("password", "Size.userForm.password");
         }
-//FIX ME fix me
+//FIX ME fix ME
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
             errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
         }
